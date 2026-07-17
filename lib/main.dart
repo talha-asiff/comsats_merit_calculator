c0c40bcf7d4cb0c573ed056b686d1668e8af6bb3
Talha Asif <sp26-bse-064@cuilahore.edu.pk>
2026-07-17 08:38:22 +0500

    form text feild with validator

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
 
-double? totalMarksSSC;
-double? marksSSC;
-double? totalMarksInter;
-double? marksInter;
-double? nts;
-double? aggregate;
+
 void main() {
   runApp(MainApp());
 }
@@ -16,8 +11,7 @@ class MainApp extends StatefulWidget {
 }
 
 class _MainAppState extends State<MainApp> {
-
-
+  
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
@@ -33,6 +27,13 @@ class MeritCalc extends StatefulWidget {
 }
 
 class _MeritCalcState extends State<MeritCalc> {
+  final inpForm = GlobalKey<FormState>();
+  double? totalMarksSSC;
+  double? marksSSC;
+  double? totalMarksInter;
+  double? marksInter;
+  double? nts;
+  double? aggregate;
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark(),
@@ -50,148 +51,247 @@ class _MeritCalcState extends State<MeritCalc> {
             ],
           ),
         ),
-        body: Column(
-          children: [
-            SizedBox(height: 20),
-            TextField(
-              keyboardType: TextInputType.numberWithOptions(),
-              decoration: InputDecoration(
-                label: Text("Total SSC Marks"),
-                fillColor: Colors.grey[850],
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
-                  borderSide: BorderSide.none,
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 2),
-                ),
-                contentPadding: EdgeInsets.symmetric(
-                  horizontal: 20,
-                  vertical: 16,
-                ),
-                prefixIcon: Icon(
-                  Icons.school_outlined,
-                  color: Colors.blueAccent,
-                ),
-              ),
-              onChanged: (x) {
-                setState(() {
-                  totalMarksSSC = double.tryParse(x)!;
-                });
-              },
-            ),
-            TextField(
-              keyboardType: TextInputType.numberWithOptions(),
-              decoration: InputDecoration(
-                label: Text("Obtained SSC Marks"),
-                fillColor: Colors.grey[850],
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
-                  borderSide: BorderSide.none,
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 2),
-                ),
-                contentPadding: EdgeInsets.symmetric(
-                  horizontal: 20,
-                  vertical: 16,
-                ),
-                prefixIcon: Icon(Icons.school, color: Colors.blueAccent),
-              ),
-              onChanged: (x) {
-                setState(() {
-                  marksSSC = double.tryParse(x)!;
-                });
-              },
-            ),
-            TextField(
-              keyboardType: TextInputType.numberWithOptions(),
-              decoration: InputDecoration(
-                label: Text("Total HSSC Marks"),
-                fillColor: Colors.grey[850],
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
-                  borderSide: BorderSide.none,
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 2),
-                ),
-                contentPadding: EdgeInsets.symmetric(
-                  horizontal: 20,
-                  vertical: 16,
-                ),
-                prefixIcon: Icon(
-                  Icons.school_outlined,
-                  color: Colors.blueAccent,
-                ),
-              ),
-              onChanged: (x) {
-                setState(() {
-                  totalMarksInter = double.tryParse(x)!;
-                });
-              },
-            ),
-            TextField(
-              keyboardType: TextInputType.numberWithOptions(),
-              decoration: InputDecoration(
-                label: Text("Obtained HSSC Marks"),
-                fillColor: Colors.grey[850],
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
-                  borderSide: BorderSide.none,
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 2),
-                ),
-                contentPadding: EdgeInsets.symmetric(
-                  horizontal: 20,
-                  vertical: 16,
-                ),
-                prefixIcon: Icon(Icons.school, color: Colors.blueAccent),
-              ),
-              onChanged: (x) {
-                setState(() {
-                  marksInter = double.tryParse(x)!;
-                });
-              },
-            ),
-            TextField(
-              keyboardType: TextInputType.numberWithOptions(),
-              decoration: InputDecoration(
-                label: Text("NTS Marks out of 100"),
-                fillColor: Colors.grey[850],
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
-                  borderSide: BorderSide.none,
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 2),
-                ),
-                contentPadding: EdgeInsets.symmetric(
-                  horizontal: 20,
-                  vertical: 16,
-                ),
-                prefixIcon: Icon(Icons.calculate, color: Colors.blueAccent),
-              ),
-              onChanged: (x) {
-                setState(() {
-                  nts = double.tryParse(x)!;
-                  aggregate =
-                      (((marksSSC! / totalMarksSSC!) * 100) * 0.1 +
-                      ((marksInter! / totalMarksInter!) * 100) * 0.4 +
-                      ((nts! / 100!) * 100) * 0.5);
-                });
-              },
-            ),
+        body: Center(
+          child: Column(
+            children: [
+              SizedBox(height: 20),
+              // TextField(
+              //   keyboardType: TextInputType.numberWithOptions(),
+              //   decoration: InputDecoration(
+              //     label: Text("Total SSC Marks"),
+              //     fillColor: Colors.grey[850],
+              //     border: OutlineInputBorder(
+              //       borderRadius: BorderRadius.circular(12),
+              //       borderSide: BorderSide.none,
+              //     ),
+              //     focusedBorder: OutlineInputBorder(
+              //       borderRadius: BorderRadius.circular(12),
+              //       borderSide: BorderSide(color: Colors.blueAccent, width: 2),
+              //     ),
+              //     contentPadding: EdgeInsets.symmetric(
+              //       horizontal: 20,
+              //       vertical: 16,
+              //     ),
+              //     prefixIcon: Icon(
+              //       Icons.school_outlined,
+              //       color: Colors.blueAccent,
+              //     ),
+              //   ),
+              //   onChanged: (x) {
+              //     setState(() {
+              //       totalMarksSSC = double.tryParse(x)!;
+              //     });
+              //   },
+              // ),
+              // TextField(
+              //   keyboardType: TextInputType.numberWithOptions(),
+              //   decoration: InputDecoration(
+              //     label: Text("Obtained SSC Marks"),
+              //     fillColor: Colors.grey[850],
+              //     border: OutlineInputBorder(
+              //       borderRadius: BorderRadius.circular(12),
+              //       borderSide: BorderSide.none,
+              //     ),
+              //     focusedBorder: OutlineInputBorder(
+              //       borderRadius: BorderRadius.circular(12),
+              //       borderSide: BorderSide(color: Colors.blueAccent, width: 2),
+              //     ),
+              //     contentPadding: EdgeInsets.symmetric(
+              //       horizontal: 20,
+              //       vertical: 16,
+              //     ),
+              //     prefixIcon: Icon(Icons.school, color: Colors.blueAccent),
+              //   ),
+              //   onChanged: (x) {
+              //     setState(() {
+              //       marksSSC = double.tryParse(x)!;
+              //     });
+              //   },
+              // ),
+              // TextField(
+              //   keyboardType: TextInputType.numberWithOptions(),
+              //   decoration: InputDecoration(
+              //     label: Text("Total HSSC Marks"),
+              //     fillColor: Colors.grey[850],
+              //     border: OutlineInputBorder(
+              //       borderRadius: BorderRadius.circular(12),
+              //       borderSide: BorderSide.none,
+              //     ),
+              //     focusedBorder: OutlineInputBorder(
+              //       borderRadius: BorderRadius.circular(12),
+              //       borderSide: BorderSide(color: Colors.blueAccent, width: 2),
+              //     ),
+              //     contentPadding: EdgeInsets.symmetric(
+              //       horizontal: 20,
+              //       vertical: 16,
+              //     ),
+              //     prefixIcon: Icon(
+              //       Icons.school_outlined,
+              //       color: Colors.blueAccent,
+              //     ),
+              //   ),
+              //   onChanged: (x) {
+              //     setState(() {
+              //       totalMarksInter = double.tryParse(x)!;
+              //     });
+              //   },
+              // ),
+              // TextField(
+              //   keyboardType: TextInputType.numberWithOptions(),
+              //   decoration: InputDecoration(
+              //     label: Text("Obtained HSSC Marks"),
+              //     fillColor: Colors.grey[850],
+              //     border: OutlineInputBorder(
+              //       borderRadius: BorderRadius.circular(12),
+              //       borderSide: BorderSide.none,
+              //     ),
+              //     focusedBorder: OutlineInputBorder(
+              //       borderRadius: BorderRadius.circular(12),
+              //       borderSide: BorderSide(color: Colors.blueAccent, width: 2),
+              //     ),
+              //     contentPadding: EdgeInsets.symmetric(
+              //       horizontal: 20,
+              //       vertical: 16,
+              //     ),
+              //     prefixIcon: Icon(Icons.school, color: Colors.blueAccent),
+              //   ),
+              //   onChanged: (x) {
+              //     setState(() {
+              //       marksInter = double.tryParse(x)!;
+              //     });
+              //   },
+              // ),
+              // TextField(
+              //   keyboardType: TextInputType.numberWithOptions(),
+              //   decoration: InputDecoration(
+              //     label: Text("NTS Marks out of 100"),
+              //     fillColor: Colors.grey[850],
+              //     border: OutlineInputBorder(
+              //       borderRadius: BorderRadius.circular(12),
+              //       borderSide: BorderSide.none,
+              //     ),
+              //     focusedBorder: OutlineInputBorder(
+              //       borderRadius: BorderRadius.circular(12),
+              //       borderSide: BorderSide(color: Colors.blueAccent, width: 2),
+              //     ),
+              //     contentPadding: EdgeInsets.symmetric(
+              //       horizontal: 20,
+              //       vertical: 16,
+              //     ),
+              //     prefixIcon: Icon(Icons.calculate, color: Colors.blueAccent),
+              //   ),
+              //   onChanged: (x) {
+              //     setState(() {
+              //       nts = double.tryParse(x)!;
+              //       aggregate =
+              //           (((marksSSC! / totalMarksSSC!) * 100) * 0.1 +
+              //           ((marksInter! / totalMarksInter!) * 100) * 0.4 +
+              //           ((nts! / 100!) * 100) * 0.5);
+              //     });
+              //   },
+              // ),
 
-            Text("${aggregate ?? 0.0}", style: TextStyle(fontSize: 40)),
-            Text("${predict(aggregate ?? 0)}"),
-          ],
+              Form(
+                key : inpForm,
+                child: Container(
+                  width: 350 * (MediaQuery.of(context).size.width >= MediaQuery.of(context).size.height ? 2 : 1),
+                  height: 500,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbtfmQr-KTl02JJQxiepDT0OevtrzTZI33HcW3M2Hh0A&s=10",),
+                      fit: BoxFit.cover
+                      ),
+                    borderRadius: BorderRadius.circular(20),
+                    border: Border.all(
+                      color: Colors.white
+                    )
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Column(
+                      children: [
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            SizedBox(
+                          width: 150 * (MediaQuery.of(context).size.width >= MediaQuery.of(context).size.height ? 2 : 1),
+                          height: 50,
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              label: Text("Total SSC Marks : ", style: TextStyle(color: Colors.white, fontSize: 10)),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20),
+                                borderSide: BorderSide(
+                                  color: const Color.fromARGB(255, 33, 51, 243),
+                                  width: 1
+                                )
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20),
+                                borderSide: BorderSide(
+                                  color: Colors.blue,
+                                  width: 1
+                                )
+                              ),
+                            ),
+                            validator: (value) {
+                              if(value == null || ((double.tryParse(value) ?? 0) < (marksSSC ?? 0) )){
+                                return "Obtained marks must be greater than or equal to Total marks";
+                              }
+                              return null;
+                            },
+                            onSaved: (newValue) {
+                              totalMarksSSC = double.tryParse(newValue!);
+                            },
+                          ),
+                        ),
+
+                        SizedBox(
+                          width: 150 * (MediaQuery.of(context).size.width >= MediaQuery.of(context).size.height ? 2 : 1),
+                          height: 50,
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              label: Text("SSC Marks : ", style: TextStyle(color: Colors.white, fontSize: 10)),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20),
+                                borderSide: BorderSide(
+                                  color: const Color.fromARGB(255, 33, 51, 243),
+                                  width: 1
+                                )
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20),
+                                borderSide: BorderSide(
+                                  color: Colors.blue,
+                                  width: 1
+                                )
+                              ),
+                            ),
+                            validator: (value) {
+                              if(value == null || ((double.tryParse(value) ?? 0) > (totalMarksSSC ?? 0) )){
+                                return "Obtained marks must be less than or equal to Total marks";
+                              }
+                              return null;
+                            },
+                            onSaved: (newValue) {
+                              marksSSC = double.tryParse(newValue!);
+                            },
+                          ),
+                        ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              
+          
+              Text("${aggregate ?? 0.0}", style: TextStyle(fontSize: 40)),
+              Text("${predict(aggregate ?? 0)}"),
+            ],
+          ),
         ),
       ),
     );
